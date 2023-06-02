4a7ba33e1e5f3411b388aa85e300e11b1deff2e4
cao quoc huy <terupery@gmail.com>
2023-06-02 20:39:33 +0700

    add database in Register

--- app/src/main/java/com/example/myjob/Login.java
+++ app/src/main/java/com/example/myjob/Login.java
@@ -1,5 +1,7 @@
 package com.example.myjob;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,15 +17,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
     TextView txt_signup;
     EditText editText_username, editText_password;
     Button btn_login;
     FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +48,7 @@ public class Login extends AppCompatActivity {
         editText_password = findViewById(R.id.txtbox_login_password);
         btn_login = findViewById(R.id.btn_login);
         mAuth = FirebaseAuth.getInstance();
+
     }
     void init(){
         String text = "You Don't have an account? <b>Sign Up</b>";
@@ -64,6 +76,9 @@ public class Login extends AppCompatActivity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
                                     Toast.makeText(Login.this, "Authentication succes.", Toast.LENGTH_SHORT).show();
                                 } else {
                                     // If sign in fails, display a message to the user.
@@ -81,5 +96,7 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/myjob/MainActivity.java
+++ app/src/main/java/com/example/myjob/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.myjob;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.play.core.integrity.e;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseFirestore db;
+    Button btn_login2;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+        btn_login2 = findViewById(R.id.btn_login2);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        init();
+    }
+
+    void init(){
+        btn_login2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> city = new HashMap<>();
+                city.put("name", "Los Angeles");
+                city.put("state", "CA");
+                city.put("country", "USA");
+
+                db.collection("Users").document(user.getUid())
+                        .set(city)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                                Toast.makeText(MainActivity.this, " succes.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                e.printStackTrace();
+                                Toast.makeText(MainActivity.this, "non succes.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 }
