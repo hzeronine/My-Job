69b53665731f8c9bbdb0571b711471abee569590
Huy Hoang <hoangkool669@gmail.com>
2023-06-08 20:48:06 +0700

    Đỗ Huy Hoàng

--- app/src/main/java/com/example/myjob/MainActivity.java
+++ app/src/main/java/com/example/myjob/MainActivity.java
@@ -4,6 +4,7 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -16,52 +17,16 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.play.core.integrity.e;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore db;
-    Button btn_login2;
-    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        db = FirebaseFirestore.getInstance();
-        btn_login2 = findViewById(R.id.btn_login2);
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        init();
-    }
-
-    void init(){
-        btn_login2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Map<String, Object> city = new HashMap<>();
-                city.put("name", "Los Angeles");
-                city.put("state", "CA");
-                city.put("country", "USA");
-
-                db.collection("Users").document(user.getUid())
-                        .set(city)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-
-                                Toast.makeText(MainActivity.this, " succes.", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                e.printStackTrace();
-                                Toast.makeText(MainActivity.this, "non succes.", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-        });
+        setContentView(R.layout.activity_saved);
     }
 }
--- app/src/main/java/com/example/myjob/SavedActivity.java
+++ app/src/main/java/com/example/myjob/SavedActivity.java
@@ -1,14 +1,25 @@
 package com.example.myjob;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class SavedActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    ArrayList<ViecLam> listViecLam;
+    ViecLamAdapter viecLamAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+        recyclerView=findViewById(R.id.recyclerview);
+        listViecLam = new ArrayList<>();
+        listViecLam.add(new ViecLam("Chuyên Viên Cao Cấp Chăm Sóc Phát triển Khách Hàng","Ngân hàng TMCP Phát triển TP.HCM (HDBank)","Tới 28 triệu","Hải Phòng , Hà Nội","as",R.drawable.img_1));
+        viecLamAdapter = new ViecLamAdapter(this,listViecLam);
+        recyclerView.setAdapter(viecLamAdapter);
     }
 }
