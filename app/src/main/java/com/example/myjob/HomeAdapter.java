ff8f429092e5697e140bb821b2cec4723b8d45cc
Dương Quang Tuyến <kasewdqt@gmail.com>
2023-06-20 21:35:32 +0700

    Revert "update detail"
    This reverts commit 1d9cb87edbe7f3c474c19fd81c2fd926ed811141.

--- app/src/main/java/com/example/myjob/HomeAdapter.java
+++ app/src/main/java/com/example/myjob/HomeAdapter.java
@@ -43,6 +43,8 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder>{
     ArrayList<BigData> bigData;
     ArrayList<List> list;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    int numberCare = 0;
     FirebaseFirestore database_saved = FirebaseFirestore.getInstance();
 
     ArrayList<String> listID_saved = listID_saved = new ArrayList<>();
@@ -76,7 +78,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder>{
             String trimmedText = originalText.substring(0, maxLength) + "...";
             holder.txt_tieuDe.setText(trimmedText);
         } else {
-            holder.txt_tieuDe.setText(constructor_home.getTitile());
+            holder.txt_tieuDe.setText(constructor_home.getSpecialized());
         }
         holder.txt_company.setText(constructor_home.getCompany_Name());
         holder.txt_diaChi.setText(constructor_home.getCity());
